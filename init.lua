91a361133a1d9f60b04b6cced836afe8e7854ce2
Sébastien De Craene <sebastien.decraene@opteon.eu>
2025-05-14 11:31:19 +0200

    add mason

--- init.lua
+++ init.lua
@@ -5,10 +5,10 @@ require('config.lazy')
 -- ====================================
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.completeopt = menu,menuone,noselect
+vim.opt.completeopt = menu, menuone, noselect
 vim.g.mapleader = " "
 
-vim.opt.clipboard = unamed,unnamedplus
+vim.opt.clipboard = unamed, unnamedplus
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -22,6 +22,10 @@ vim.opt.swapfile = false
 -- hide the cmdline behind the status bar
 -- vim.opt.cmdheight=1
 -- vim.opt.fillchars = { eob = ' ' }
+--
+
+
+vim.opt.textwidth = 100
 
 -- colorscheme
 require('catppuccin').setup({
@@ -31,10 +35,10 @@ vim.cmd.colorscheme "catppuccin"
 
 
 -- Hide line numbers in terminal
--- 
+--
 vim.api.nvim_create_autocmd("TermOpen", {
-  pattern = "*",
-  command = "setlocal nonumber norelativenumber signcolumn=no",
+    pattern = "*",
+    command = "setlocal nonumber norelativenumber signcolumn=no",
 })
 
 
@@ -53,12 +57,13 @@ vim.keymap.set('n', '<C-i>', ':Neotree focus<CR>', { noremap = true, silent = tr
 
 -- code comments
 require('todo-comments').setup {}
-vim.keymap.set("x", "gcc", function() require("Comment.api").toggle.linewise(vim.fn.visualmode()) end, { noremap = true, silent = true })
+vim.keymap.set("x", "gcc", function() require("Comment.api").toggle.linewise(vim.fn.visualmode()) end,
+    { noremap = true, silent = true })
 
 
 -- code action
 vim.keymap.set("n", "<C-k>", function()
-	require("tiny-code-action").code_action()
+    require("tiny-code-action").code_action()
 end, { noremap = true, silent = true })
 
 --
@@ -86,58 +91,72 @@ require('nvim-autopairs').setup {}
 
 -- HTML
 vim.lsp.config['vscode-html-language-server'] = {
-     cmd = { 'vscode-html-language-server', '--stdio' },
+    cmd = { 'vscode-html-language-server', '--stdio' },
     filetypes = { 'html', 'templ' },
     single_file_support = true,
     settings = {},
     init_options = {
-      provideFormatter = true,
-      embeddedLanguages = { css = true, javascript = true },
-      configurationSection = { 'html', 'css', 'javascript' },
+        provideFormatter = true,
+        embeddedLanguages = { css = true, javascript = true },
+        configurationSection = { 'html', 'css', 'javascript' },
     }
 }
 
 -- TYPESCRIPT LSP
-vim.lsp.enable('typescript-language-server')
+require('lspconfig').ts_ls.setup {}
+-- vim.lsp.config['typescript-language-server'] = {
+-- 	cmd = { 'typescript-language-server', '--stdio' },
+-- }
+-- vim.lsp.enable('typescript-language-server')
 
 -- LUA LSP
 vim.lsp.config['luals'] = {
-	cmd = { 'lua-language-server' },
-	filetypes = { 'lua' }
+    cmd = { 'lua-language-server' },
+    filetypes = { 'lua' }
 }
 
- -- Set up nvim-cmp.
+
+require("mason").setup()
+
+require("mason-lspconfig").setup {
+    automatic_enable = {
+        "lua_ls",
+        "ts_ls"
+    }
+}
+
+
+-- Set up nvim-cmp.
 local cmp = require('cmp')
 
 cmp.setup({
     snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-      end,
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        end,
     },
     window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-      ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-      ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-      ["<C-Space>"] = cmp.mapping.complete()
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete()
     }),
     sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'vsnip' }, -- For vsnip users.
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' }, -- For vsnip users.
     }, {
-      { name = 'buffer' },
+        { name = 'buffer' },
     })
-  })
-
+})
--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -1,5 +1,10 @@
-return {
+return
+    {
   "pmizio/typescript-tools.nvim",
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {},
+  config = function()
+    require("typescript-tools").setup({})
+  end,
 }
+
+
